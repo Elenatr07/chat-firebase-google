0bb91f33d1a01dea56e24a06a8f1b155ebd9acd5
Elenatr07 <e.turkina2010@yandex.ru>
2023-01-01 04:58:21 +1000

    Update Chat.jsx

--- chat/src/components/Chat.jsx
+++ chat/src/components/Chat.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Context } from '../index';
+import { Container, Grid, Button, TextField } from '@material-ui/core';
+
 
 const Chat = () => {
+    const { auth, firestore } = useContext(Context)
+    const [user] = useAuthState(auth)
     return (
-        <div>
-            Chat
-        </div>
+        <Container>
+            <Grid container
+                justifyContent={'center'}
+                style={{ height: window.innerHeight - 50, marginTop: 20 }}>
+                <div style={{ width: '80%', height: '60vh', border: '1px solid #3f51b5', overflow: 'auto' }}>
+                </div>
+                <Grid
+                    container
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    style={{ width: '80%' }}
+                >
+                    <TextField
+                        id="outlined-basic" label="Your message" variant="outlined"
+                        maxRows={2}
+                        style={{ width: '80%' }}
+                    />
+                    <Button variant="contained" color="primary" style={{ width: '20%', height: '55px' }}>Send</Button>
+                </Grid>
+
+            </Grid>
+        </Container>
     );
 };
 
